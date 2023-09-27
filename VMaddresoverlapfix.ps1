6af195cec27db14bd1b53840469923f8a276eaac
Mahboob Ali <alimizan@hotmail.com>
2023-09-27 14:21:30 +0100

    Update VMaddresoverlapfix.ps1

--- VMaddresoverlapfix.ps1
+++ VMaddresoverlapfix.ps1
@@ -1,27 +1,42 @@
-﻿# Source and destination VNet resource groups 
-$sourceRG = "sourceRG"
-$destRG = "destRG"
+az vm stop --resource-group rg01 --name VM01
 
-# Source and destination VNet names
-$sourceVNet = "sourceVNet" 
-$destVNet = "destVNet"
+# Resource group name 
+rg="rg01"
 
-# Get the VM 
-$vm = Get-AzVM -Name "vmName" -ResourceGroupName $sourceRG
+# Existing VNet and VM details
+vnetName="vnet01"
+vmName="vm01" 
 
-# Get destination subnet ID
-$destSubnetID = (Get-AzVirtualNetwork -Name $destVNet -ResourceGroupName $destRG).Subnets[0].Id
+# New address space and subnet details
+$addressPrefix="10.1.0.0/16" 
+$subnetName="db"
+$subnetPrefix="10.1.0.0/24"
 
-# Detach NIC from VM
-$nic = Get-AzNetworkInterface -ResourceGroupName $sourceRG -Name $vm.NetworkProfile.NetworkInterfaces.Id.Split('/')[-1] 
-$vm = Remove-AzVMNetworkInterface -VM $vm -NetworkInterfaceIDs $nic.Id
+# Get the VNet 
+vnet=$(az network vnet show --resource-group $rg --name $vnetName --query id -o tsv)
 
-# Attach new NIC to VM
-$nic = New-AzNetworkInterface -ResourceGroupName $destRG -Location $vm.Location -SubnetId $destSubnetID -Name $vm.Name 
-$vm = Add-AzVMNetworkInterface -VM $vm -Id $nic.Id
+# Add the address space
+az network vnet update --ids $vnet --address-prefixes $addressPrefix
 
-# Update the VM
-Update-AzVM -ResourceGroupName $destRG -VM $vm
+# Create the new subnet
+az network vnet subnet create --vnet-name $vnetName --resource-group $rg --name $subnetName --address-prefixes $subnetPrefix
 
-# Delete old NIC
-Remove-AzNetworkInterface -Name $vm.Name -ResourceGroupName $sourceRG
+# Create the NIC
+az network nic create --resource-group $rg --vnet-name $vnetName --subnet $subnetName -n nic1 
+
+# Attach NIC to VM
+az vm nic add --resource-group $rg --vm-name $vmName --nics nic1
+
+#Detach old nic
+az vm nic remove --resource-group rg01 --vm-name vm01 --nics oldnic
+
+#Remove Old Subnet
+az network vnet subnet delete --resource-group rg01 --vnet-name vnet01 --name oldsubnet
+
+#Remove old address space 
+### I would recommend doing this via the CLI if this is a production vnet otherwise follow the below code replace "10.2.0.0/16" with the range you want to remove)
+
+az network vnet show -g myResourceGroup -n myVnet --query addressSpace.addressPrefixes
+addressPrefixes=($(az network vnet show -g myResourceGroup -n myVnet --query addressSpace.addressPrefixes -o tsv))
+addressPrefixes[${!addressPrefixes[@]}]="10.2.0.0/16"
+az network vnet update -g rg01 -n vnet01 --address-prefixes ${addressPrefixes[@]}
